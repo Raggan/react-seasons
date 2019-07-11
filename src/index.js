534bb645a124c6dfdb4b9557ac3d196a777b4761
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-11 10:28:52 +0200

    error handling

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { thisTypeAnnotation } from "@babel/types";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
     );
-    return <div>Latitude: </div>;
   }
 }
 
