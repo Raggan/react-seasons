73a149967b90434c1268ce2c3b6c33db883478d7
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-11 10:32:43 +0200

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { thisTypeAnnotation } from "@babel/types";
 
 class App extends React.Component {
   constructor(props) {
@@ -23,13 +22,15 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading</div>;
   }
 }
 
